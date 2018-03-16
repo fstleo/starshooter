a7a149ce806a04d2d26ad29d04c22bd58d0953dc
Lev Bespalov <lbespalov@saber3d.ru>
2018-03-16 18:20:13 +0300

    Unity project & graphic

---
+++ StarShooter.Unity/Audio/PlayerPrefsAudioSettingsController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using StarShooter.Audio.Settings;
+
+namespace StarShooter.Unity.Audio
+{
+    public class PlayerPrefsAudioSettingsController : BasicAudioSettingsController
+    {
+        private const string AudioVolumeKey = "audio_volume";
+        private const string MusicVolumeKey = "music_volume";
+
+        protected override float _audioVolume
+        {
+            get
+            {
+                return PlayerPrefs.GetFloat(AudioVolumeKey, 1);
+            }
+            set
+            {
+                PlayerPrefs.SetFloat(AudioVolumeKey, value);
+            }
+        }
+
+        protected override float _musicVolume
+        {
+            get
+            {
+                return PlayerPrefs.GetFloat(MusicVolumeKey, 1);
+            }
+            set
+            {
+                PlayerPrefs.SetFloat(MusicVolumeKey, value);
+            }
+        }
+
+    }
+}
---
+++ StarShooter.Unity/Input/UpdateInputManager.cs
@@ -0,0 +1,16 @@
+﻿using StarShooter.Input.InputManager;
+using StarShooter.Input.Interfaces;
+using Zenject;
+
+namespace StarShooter.Unity.Input
+{
+    public class UpdateInputManager : InputManager, ITickable
+    {
+        public UpdateInputManager(IInputSettings settings, INativeInput input) : base(settings, input) { }
+
+        public void Tick()
+        {
+            CheckKeys();
+        }
+    }
+}
