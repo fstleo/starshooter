a4ff2b6464a99aef123cc2dbb9560e0f566b2f93
Lev Bespalov <lbespalov@saber3d.ru>
2018-03-30 12:58:10 +0300

    Fix scene hierarchy, fix state changes

---
+++ StarShooterProject/Assets/Scripts/Installers/SceneManagementInstaller.cs
@@ -0,0 +1,12 @@
+﻿using Zenject;
+
+namespace StarShooter.GameManagement.SceneManagement.Installer
+{
+    public class SceneManagementInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+            Container.Bind<ISceneManager>().To<UnitySceneManager>().AsSingle();
+        }
+    }
+}
---
+++ StarShooterProject/Assets/Scripts/Utils/Preloader.cs
@@ -0,0 +1,14 @@
+﻿using StarShooter.GameManagement;
+using UnityEngine;
+using Zenject;
+
+public class Preloader : MonoBehaviour
+{
+
+    [Inject]
+    void Init(IAppStateManager stateManager, ISceneManager sceneManager)
+    {
+        stateManager.CurrentState = AppState.MainMenu;
+        sceneManager.LoadScene("1. MainMenu");
+    }
+}
